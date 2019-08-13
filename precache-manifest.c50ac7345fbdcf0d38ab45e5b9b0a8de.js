54cf163846aef7acfddc90fed4e1f1aeb6cc49d4
Urvashi Uberoy <uuberoy@Chetans-MBP.lan>
2019-08-13 20:17:19 +0530

    Updates

---
+++ precache-manifest.c50ac7345fbdcf0d38ab45e5b9b0a8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9a9c84a6a1b6f39a2a1d3bd128457cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6592b40eaf46b09ed1f",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1b4d327e10de82eb92f7",
+    "url": "/static/js/2.a31939dd.chunk.js"
+  },
+  {
+    "revision": "a6592b40eaf46b09ed1f",
+    "url": "/static/js/main.de5e5bbd.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.c50ac7345fbdcf0d38ab45e5b9b0a8de.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
