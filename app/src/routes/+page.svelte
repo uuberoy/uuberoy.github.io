6f6326e84270a89e60800cbd324afc9f8d0d3bc1
Urvashi Uberoy <urvashi.uberoy@nytimes.com>
2023-09-30 18:54:56 -0400

    first pass

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,2 +1,104 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <h1 class="hed">Urvashi Uberoy</h1>
+  <p class="graf">
+    I'm currently a newsroom engineer on the <a
+      href="https://github.com/newsdev/about-int">Interactive News desk</a
+    > at The New York Times.
+  </p>
+  <p class="graf">
+    <a href="https://twitter.com/urvashiuberoy">twitter</a> |
+    <a href="urvashi.uberoy@nytimes.com">email</a>
+  </p>
+
+  <section>
+    <h2 class="sub-hed">portfolio</h2>
+    <ul>
+      <li>
+        <a
+          href="https://www.nytimes.com/interactive/2023/07/21/nyregion/nyc-developers-private-owned-public-spaces.html"
+          >New Yorkers Got Broken Promises. Developers Got 20 Million Sq. Ft.</a
+        >
+        <p class="summary">
+          I pitched and reported this data-driven investigation of New York City's privately owned public
+          spaces. Developers gained bonus floor area worth ~$10 billion in
+          exchange for providing public spaces. But half of the buildings were
+          found to be violating the agreement.
+        </p>
+      </li>
+      <li>
+        reader-sourced reporting
+        <p class="summary">
+          I am the technical lead of The Times' reader-sourced reporting tool that is used to
+          create callouts. In the last year, we have used <a href="https://rd.nytimes.com/research/natural-language-understanding">NLP models</a> and mapping tools to help the newsroom analyze incoming submissions.
+        </p>
+      </li>
+      <li>
+        <a
+          href="https://www.nytimes.com/interactive/2023/upshot/womens-world-cup-standings.html"
+          >2023 world cup</a
+        >
+        <p class="summary">
+          World Cup scenarios, scoreboards and draws, presented by
+          Interactive News, the Upshot and Digital News Design.
+        </p>
+      </li>
+      <li>
+        <a
+          href="https://www.nytimes.com/interactive/2022/11/08/us/elections/results-key-races.html"
+          >2022 midterm election results</a
+        >
+        <p class="summary">
+          I worked on live alerts and race calls for the 2022 midterm elections,
+          in collaboration with Graphics and Digital News Design.
+        </p>
+      </li>
+    </ul>
+  </section>
+</div>
+
+<style>
+  .container {
+    margin: auto;
+    font-family: Georgia, "Times New Roman", Times, serif;
+    max-width: 600px;
+    color: #121212;
+    font-size: 14px;
+  }
+
+  .hed,
+  .sub-hed {
+    font-weight: normal;
+    font-size: 16px;
+    text-transform: lowercase;
+  }
+
+  .hed {
+    margin-top: 33%;
+  }
+
+  .sub-hed {
+    margin-top: 60px;
+  }
+
+  a {
+    color: inherit;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin-bottom: 10px;
+  }
+
+  .summary {
+    margin-top: 5px;
+    color: #888888;
+  }
+</style>
